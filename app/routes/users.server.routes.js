9946d8cf7aa7a3b2cbd976ed49c6b8492e1cc354
ryansalvador <ryansalvador@gmail.com>
2016-07-25 18:52:18 +0800

    Oauth Passport
    Chapter 15

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -14,5 +14,36 @@ module.exports = function(app) {
        failureFlash: true
      }));
 
+  app.get('/oauth/facebook', passport.authenticate('facebook', {
+    failureRedirect: '/signin'
+  }));
+
+  app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
+    failureRedirect: '/signin',
+    successRedirect: '/'
+  }));
+
+  app.get('/oauth/twitter', passport.authenticate('twitter', {
+    failureRedirect: '/signin'
+  }));
+
+  app.get('/oauth/twitter/callback', passport.authenticate('twitter', {
+    failureRedirect: '/signin',
+    successRedirect: '/'
+  }));
+
+  app.get('/oauth/google', passport.authenticate('google', {
+    failureRedirect: '/signin',
+    scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+    ],
+  }));
+
+  app.get('/oauth/google/callback', passport.authenticate('google', {
+    failureRedirect: '/signin',
+    successRedirect: '/'
+  }));
+
   app.get('/signout', users.signout);
 };
