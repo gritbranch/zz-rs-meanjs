355c7962f9007a2f14e0730ea2970b608ee1fff2
ryansalvador <ryansalvador@gmail.com>
2016-07-27 17:35:14 +0800

    AngularJS Authentication Service
    Chapter 19

---
+++ public/js/application.js
@@ -0,0 +1,24 @@
+var mainApplicationModuleName = 'mean';
+
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'users', 'example']);
+
+// Add Hashbang so they are indexable by search engine crawlers and for SEO purposes.
+mainApplicationModule.config(['$locationProvider',
+  function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+  }
+]);
+
+// Fixing facebook bug with redirect
+if (window.location.hash === '#_=_') window.location.hash = '#!';
+
+// Fixing google bug with redirect
+if (window.location.href[window.location.href.length - 1] === '#' &&
+    // for just the error url (origin + /#)
+    (window.location.href.length - window.location.origin.length) === 2) {
+    window.location.href = window.location.origin + '/#!';
+}
+
+angular.element(document).ready(function() {
+  angular.bootstrap(document, [mainApplicationModuleName]);
+});
