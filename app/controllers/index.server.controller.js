57908bd380139a1baa37fd4ef2ea59bd7e152931
ryansalvador <ryansalvador@gmail.com>
2016-08-05 19:46:13 +0800

    Added Template

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,6 +1,6 @@
 exports.render = function(req, res) {
   res.render('index', {
-    title: 'Mean Stack Reference',
+    title: 'MEAN Stack',
     user: JSON.stringify(req.user)
   });
 };
--- public/js/controllers/course.client.controller.js
+++ public/js/controllers/course.client.controller.js
@@ -1,6 +1,14 @@
 angular.module('course').controller('CourseController', ['$scope', 'Authentication', 'ChapterFactory',
   function($scope, Authentication, ChapterFactory) {
     $scope.name = Authentication.user ? Authentication.user.fullName : 'MEAN Application';
+    
     $scope.title = ChapterFactory;
+
+    $scope.myStyle={'background-color':'blue'};
+
+    $scope.work1={'background-image': 'url(img/work_1.jpg)'};
+    $scope.work2={'background-image': 'url(img/work_2.jpg)'};
+    $scope.work3={'background-image': 'url(img/work_3.jpg)'};
+    $scope.work4={'background-image': 'url(img/work_4.jpg)'};
   }
 ]);
