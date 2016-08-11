254ae010bdd29cdaffc5b4465637bdb0cfc82454
ryansalvador <ryansalvador@gmail.com>
2016-08-11 17:28:26 +0800

    Change prod parameters 2.0

--- config/env/production.js
+++ config/env/production.js
@@ -4,20 +4,20 @@
 
 module.exports = {
   db: 'mongodb://ryansalvador:xxxPA50@ds059692.mongolab.com:59692/ryansalvadordb',
-  sessionSecret: 'developmentSessionSecret',
+  sessionSecret: 'productionSessionSecret',
   facebook: {
     clientID: '1061065367261191',
     clientSecret: '27831c304b199565a67a38659dae4958',
-    callbackURL: 'https://ryansalvador.heroku.com/oauth/facebook/callback'
+    callbackURL: 'http://localhost:3000/oauth/facebook/callback'
   },  
   twitter: {
     clientID: '57k6XmQsmXIN4BcXgv5qOmX5Y',
     clientSecret: 'UDMZywySzHDs9FcAJE73zut1PqS47rfI5u2km3oDQxFNsPxNAk',
-    callbackURL: 'https://ryansalvador.heroku.com/oauth/twitter/callback'
+    callbackURL: 'https://ryansalvador.herokuapp.com/oauth/twitter/callback'
   },
   google: {
     clientID: '232318554867-u9klo7c5s79ta3hb2q2fd5m69lomt8gs.apps.googleusercontent.com',
     clientSecret: 'JUQa4L4-stYCcoad-fnd48DF',
-    callbackURL: 'https://ryansalvador.heroku.com/oauth/google/callback'
+    callbackURL: 'https://ryansalvador.herokuapp.com/oauth/google/callback'
   }
 };
